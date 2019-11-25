48887977a4d32eb27a6d54c04cc591918d4ac1a4
phelanm <phelanm2000@gmail.com>
2019-11-25 14:26:45 -0800

    Started implementing Spline Class

--- src/software/new_geom/spline.cpp
+++ src/software/new_geom/spline.cpp
@@ -1,22 +1,16 @@
 #include "software/new_geom/spline.h"
-/*
+
 
 Spline::Spline(const std::vector<Point>& points, unsigned int order) : knots(points)
 {
-    initLinearSegments(points);
-
     makeSegments(points, order);
-
 }
 
 Spline::Spline(const std::initializer_list<Point>& points, unsigned int order) : knots(points)
 {
-    initLinearSegments(points);
-
     makeSegments(points, order);
 }
 
-//HERE
 Point Spline::valueAt(double val) const
 {
     if (val < 0.0 || val > 1.0)
@@ -83,74 +77,72 @@ Construct a polynomial from coefficients
  *
 */
 
+//Make a segment and add it to segment vector
 /*
+Polynomial poly_x = Polynomial(std::make_pair(input_start, points[i - 1].x()),
+                               std::make_pair(input_end, points[i].x()));
+Polynomial poly_y = Polynomial(std::make_pair(input_start, points[i - 1].y()),
+                               std::make_pair(input_end, points[i].y()));
+segments.push_back(SplineSegment(poly_x, poly_y, input_start, input_end));
+*/
 
-//Maria's Function!!!
 //void insert_element (size_type i, size_type j, const_reference t)
-void Spline::makeSegments(const std::vector<Point>& points)
+
+void Spline::initSegments(const std::vector<Point>& points, unsigned int order)
 {
-    if (points.size() == 0)
-        {
-        throw std::runtime_error("Cannot create spline with no points.");
-        }
+    using namespace boost::numeric::ublas;
+    if (points.size() == 0 || points.size() == 1)
+    {
+        throw std::runtime_error("Cannot create spline with less than two points.");
+    }
     else if (points.size() > 1)
-        {
-        //only splines with more than one point can have spline segments
-        for (size_t i = 1; i < points.size(); i++)
+    {
+        for (size_t m = 1; m < points.size(); m++)
         {
 
-        }
+            fillInterpolationMatrix(points[m], points[m+1], order);
 
-            for (size_t i = 1; i < points.size(); i++)
+            //Get start and end points to interpolate between
+            startX = points[m].x();
+            startY = points[m].y();
+            endX = points[m + 1].x();
+            endY = points[m + 1].y();
+
+            //initialize empty matrix
+            matrix<double> m(order + 1, order + 1);
+
+            //initialize first row
+            double exp = order;
+            for (unsigned int j = 0; j < m.size2(); j++)
             {
-                double input_start = (i - 1) / ((double)points.size() - 1);
-                double input_end   = (i) / ((double)points.size() - 1);
-
-                Polynomial poly_x = Polynomial(std::make_pair(input_start, points[i - 1].x()),
-                                               std::make_pair(input_end, points[i].x()));
-                Polynomial poly_y = Polynomial(std::make_pair(input_start, points[i - 1].y()),
-                                               std::make_pair(input_end, points[i].y()));
-                segments.push_back(SplineSegment(poly_x, poly_y, input_start, input_end));
+                m(0, j) = pow(endX, exp);
+                exp--;
             }
 
-        }
-}
+            //initialize second row
+            double exp = order;
+            for (unsigned int j = 0; j < m.size2(); j++)
+            {
+                m(1, j) = pow(startX, exp);
+                exp--;
+            }
+
+            //initialize third to final rows
+            double exp = order;
+            for (unsigned int i = 2; i < m.size1(); i++)
+            {
+                for(unsigned int j = 0; j < m.size2(); j++)
+                {
+                    m(i, j) = m(i-1, j)/startX      pow(startX, exp);
+                    exp--;
+                }
+            }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-void Spline::initLinearSegments(const std::vector<Point>& points)
-{
-    if (points.size() == 0)
-    {
-        throw std::runtime_error("Cannot create spline with no points");
-    }
-    else if (points.size() > 1)
-    {
-        // only splines with more than one point can have segments
-        for (size_t i = 1; i < points.size(); i++)
-        {
-            double input_start = (i - 1) / ((double)points.size() - 1);
-            double input_end   = (i) / ((double)points.size() - 1);
-
-            Polynomial poly_x = Polynomial(std::make_pair(input_start, points[i - 1].x()),
-                                           std::make_pair(input_end, points[i].x()));
-            Polynomial poly_y = Polynomial(std::make_pair(input_start, points[i - 1].y()),
-                                           std::make_pair(input_end, points[i].y()));
-            segments.push_back(SplineSegment(poly_x, poly_y, input_start, input_end));
         }
     }
 }
 
-*/
+void fillInterpolationMatrix(Point start, Point end, unsigned int order){
+
+}
--- src/software/new_geom/spline.h
+++ src/software/new_geom/spline.h
@@ -1,8 +1,10 @@
 #pragma once
-/*
-#include <vector>
-//#include <Eigen/Dense>
 
+#include <eigen/dense>
+//alread in repo
+
+#include <vector>
+#include <math.h>
 #include <boost/numeric/ublas/matrix.hpp>
 #include "software/geom/polynomial.h"
 #include "software/new_geom/point.h"
@@ -10,21 +12,10 @@
 /**
  * A Spline is a parameterized piecewise function
  * composed of Polynomials of set order between Points
-
-class Spline
-{
+ */
+class Spline {
 public:
     Spline() = delete;
-    
-    /**
-     * Construct a spline by drawing Polynomials of set order between
-     * consecutive Points
-     *
-     * @throws std::invalid_argument if points.size() == 0
-     *
-     * @param points Points on the spline
-
-    explicit Spline(const std::vector<Point>& points, unsigned int order);
 
     /**
      * Construct a spline by drawing Polynomials of set order between
@@ -33,8 +24,18 @@ public:
      * @throws std::invalid_argument if points.size() == 0
      *
      * @param points Points on the spline
+     */
+    explicit Spline(const std::vector<Point> &points, unsigned int order);
 
-    Spline(const std::initializer_list<Point>& points, unsigned int order);
+    /**
+     * Construct a spline by drawing Polynomials of set order between
+     * consecutive Points
+     *
+     * @throws std::invalid_argument if points.size() == 0
+     *
+     * @param points Points on the spline
+     */
+    Spline(const std::initializer_list<Point> &points, unsigned int order);
 
     /**
      * Calculates the value of spline evaluated at value val
@@ -44,69 +45,66 @@ public:
      * @throws std::invalid_argument if val is outside of domain
      *
      * @return value of spline evaluated at value val
-     *      * if not defined by a spline then return closest start/end point
-
+     * if not defined by a spline then return closest start/end point
+     */
     Point valueAt(double val) const;
 
     /**
      * Gets the number of knots in the spline including start and end points
      *
      * @return size of the spline
-
+     */
     size_t size(void) const;
 
     /**
      * Gets knots in the spline including start and end points
      *
      * @return knots in the spline
-
+     */
     const std::vector<Point> getKnots(void) const;
 
     /**
      * Gets start point of spline
      *
      * @return start point of spline
-
+     */
     const Point startPoint(void) const;
 
     /**
      * Gets end point of spline
      *
      * @return end point of spline
-
+     */
     const Point endPoint(void) const;
 
 private:
-    class SplineSegment
-    {
+    class SplineSegment {
     public:
         SplineSegment(Polynomial x, Polynomial y, double start, double end)
-                : x(x), y(y), start(start), end(end)
-        {
+                : x(x), y(y), start(start), end(end) {
         }
+
         const Polynomial x;
         const Polynomial y;
         const double start;
         const double end;
     };
 
-    // segments represent the polynomials that interpolate between
-    // time at index to index + 1
+    // segments represent the polynomials that interpolate between points
     std::vector<SplineSegment> segments;
 
     // points that connect segments
     const std::vector<Point> knots;
 
     /**
-     * Initialize segments with points.size() - 1 linear segments interpolating
-     * the points
+     * Initialize (points.size() - 1) segments of order 'order' interpolating the points
+     * 'Order' is the value of the largest exponent in the segment
      * Initialize start and end points
      *
-     * @throws std::runtime_error if points.size() == 1
+     * @throws std::runtime_error if points.size() < 2
      *
      * @param points points to interpolate
-
-    void initLinearSegments(const std::vector<Point>& points);
+     * @param order order of interpolating segments
+     */
+    void initSegments(const std::vector<Point> &points, unsigned int order);
 };
-
-*/
